434100e21612b416574d6b740590701a609af7ee
I-am-VarunM <85482884+I-am-VarunM@users.noreply.github.com>
2025-03-19 13:20:46 +0530

    Update tppe.v

--- tppe.v
+++ tppe.v
@@ -146,13 +146,17 @@ module tppe #(
         .result_1(result_1),
         .result_2(result_2),
         .result_3(result_3),
+        .result_4(result_4),
+        .result_5(result_5),
+        .result_6(result_6),
+        .result_7(result_7),
         .result_valid(result_valid)
     );
     
     LIF_Model #(
     .T(TIMESTEPS),  
     .Q(CORRECTION_ACC_WIDTH)  
-    )LIF_Model_inst(.result_val(result_valid),.clk(clk),.start(1'b1), .rst_n(rst_lif), .input_data({result_7, result_6, result_5, result_4, result_3, result_2, result_1, result_0}), .threshold(8'b0000011111), .spike_out(lif_output),.lif_done(lif_done));
+)LIF_Model_inst(.result_val(result_valid),.clk(clk),.start(1'b1), .rst_n(rst_lif), .input_data({result_7, result_6, result_5, result_4, result_3, result_2, result_1, result_0}), .threshold(10'b0000011111), .spike_out(lif_output),.lif_done(lif_done));
     
     // Connect FIFO read enables from accumulator to laggy prefix
     // In the current implementation of accumulator_correction, we don't have separate
